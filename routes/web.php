296c17cee746062767e59a916337a2bd44ce7523
Thanish Ahamed <thanishahamed@Thanishs-MacBook-Air.local>
2021-10-11 18:17:35 +0530

    Admin view creation setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
